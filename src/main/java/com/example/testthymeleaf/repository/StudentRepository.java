a49306f00226c83f3e52d3660d33dd3290e27853
indrawijayakusuma <indrawk56@gmail.com>
2023-03-28 09:38:10 +0700

    sebelum test fitur searching

--- src/main/java/com/example/testthymeleaf/repository/StudentRepository.java
+++ src/main/java/com/example/testthymeleaf/repository/StudentRepository.java
@@ -1,9 +1,12 @@
 package com.example.testthymeleaf.repository;
 
 import com.example.testthymeleaf.entity.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
+    Page<Student> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
 }
